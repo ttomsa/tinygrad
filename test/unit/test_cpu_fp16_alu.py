ec4bf2652a08b82b679d18d7b1665f04beb21097
ttomsa <tomasvsilva8@gmail.com>
2025-06-16 01:45:23 +0100

    test

---
+++ test/unit/test_cpu_fp16_alu.py
@@ -0,0 +1,13 @@
+import unittest
+from tinygrad import Tensor, dtypes, Context
+from tinygrad.device import Device
+from tinygrad.helpers import OSX
+
+class TestFloat16Alu(unittest.TestCase):
+  @unittest.skipUnless(Device.DEFAULT == "CPU" and OSX, "")
+  def test_cpu(self):
+    with Context(DEBUG=7):
+      a = Tensor([1], dtype=dtypes.float16)
+      b = Tensor([2], dtype=dtypes.float16)
+      c = (a + b).realize()
+    
