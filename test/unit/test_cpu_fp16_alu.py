33ad6d395e89992316e84089533da891e748b9b5
ttomsa <tomasvsilva8@gmail.com>
2025-06-16 18:07:05 +0100

    are if it's vectorized?

--- test/unit/test_cpu_fp16_alu.py
+++ test/unit/test_cpu_fp16_alu.py
@@ -6,8 +6,8 @@ from tinygrad.helpers import OSX
 class TestFloat16Alu(unittest.TestCase):
   @unittest.skipUnless(Device.DEFAULT == "CPU" and OSX, "")
   def test_cpu(self):
-    a = Tensor([1], dtype=dtypes.float16)
-    b = Tensor([2], dtype=dtypes.float16)
+    a = Tensor([1,2,3,4], dtype=dtypes.float16)
+    b = Tensor([1,2,3,4], dtype=dtypes.float16)
     c = (a + b).realize()
 
 if __name__ == "__main__":
