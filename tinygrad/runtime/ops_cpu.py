41d30809982668d4ddce538d10e274d8b1497159
ttomsa <tomasvsilva8@gmail.com>
2025-06-16 23:07:41 +0100

    what about now?

--- tinygrad/runtime/ops_cpu.py
+++ tinygrad/runtime/ops_cpu.py
@@ -1,8 +1,9 @@
-import functools, platform, subprocess, sys
+import functools, platform, subprocess, sys, ctypes
 from tinygrad.helpers import capstone_flatdump, getenv, OSX
 from tinygrad.device import Compiled, Compiler, MallocAllocator, CPUProgram
 from tinygrad.runtime.support.elf import jit_loader
 from tinygrad.renderer.cstyle import ClangRenderer
+from tinygrad.runtime.autogen.llvm import LLVMGetHostCPUName
 
 class ClangJITCompiler(Compiler):
   def __init__(self, cachekey="compile_clang_jit"): super().__init__(cachekey)
@@ -11,8 +12,8 @@ class ClangJITCompiler(Compiler):
     # -fno-math-errno is required for __builtin_sqrt to become an instruction instead of a function call
     # x18 is a reserved platform register. It is clobbered on context switch in macos and is used to store TEB pointer in windows on arm, don't use it
     target = 'x86_64' if sys.platform == 'win32' else platform.machine()
-    target += '-apple' if OSX else '-none'
-    args = ['-march=native', f'--target={target}-unknown-elf', '-O2', '-fPIC', '-ffreestanding', '-fno-math-errno', '-nostdlib', '-fno-ident']
+    cpu = f"-mcpu={ctypes.string_at(LLVMGetHostCPUName()).decode()}"
+    args = [cpu, f'--target={target}-none-unknown-elf', '-O2', '-fPIC', '-ffreestanding', '-fno-math-errno', '-nostdlib', '-fno-ident']
     arch_args = ['-ffixed-x18'] if target == 'arm64' else []
     obj = subprocess.check_output([getenv("CC", 'clang'), '-c', '-x', 'c', *args, *arch_args, '-', '-o', '-'], input=src.encode('utf-8'))
     return jit_loader(obj)
