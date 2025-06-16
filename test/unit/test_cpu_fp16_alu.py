e0e85ff1ea2bd9c53f48f5d237de1d54c2ccb7bd
ttomsa <tomasvsilva8@gmail.com>
2025-06-16 03:23:23 +0100

    interdimensional l

--- test/unit/test_cpu_fp16_alu.py
+++ test/unit/test_cpu_fp16_alu.py
@@ -1,15 +1,14 @@
 import unittest
-from tinygrad import Tensor, dtypes, Context
+from tinygrad import Tensor, dtypes
 from tinygrad.device import Device
 from tinygrad.helpers import OSX
 
 class TestFloat16Alu(unittest.TestCase):
   @unittest.skipUnless(Device.DEFAULT == "CPU" and OSX, "")
   def test_cpu(self):
-    with Context(DEBUG=7):
-      a = Tensor([1], dtype=dtypes.float16)
-      b = Tensor([2], dtype=dtypes.float16)
-      c = (a + b).realize()
+    a = Tensor([1], dtype=dtypes.float16)
+    b = Tensor([2], dtype=dtypes.float16)
+    c = (a + b).realize()
 
 if __name__ == "__main__":
   unittest.main()
